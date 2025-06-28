44af9f633d5c4f36c4a0dc73600f3dd346b6d4e4
karl.sun <karl.sun@facilityone.cn>
2025-06-28 13:33:28 +0800

    Initial Commit

---
+++ XSshowTabForm/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  XSshowTabForm
+//
+//  Created by xinsun on 2025/6/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ XSshowTabFormTests/XSshowTabFormTests.swift
@@ -0,0 +1,17 @@
+//
+//  XSshowTabFormTests.swift
+//  XSshowTabFormTests
+//
+//  Created by xinsun on 2025/6/28.
+//
+
+import Testing
+@testable import XSshowTabForm
+
+struct XSshowTabFormTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
